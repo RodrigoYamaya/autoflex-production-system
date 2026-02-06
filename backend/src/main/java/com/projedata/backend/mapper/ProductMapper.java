c89918b7f6deccd5bec8d6884939d97ee03361fe
rodrigo <kakakpegado@hotmail.com>
2026-02-06 17:43:52 -0300

    feat: create MapStruct mappers for RawMaterial and Product

--- backend/src/main/java/com/projedata/backend/mapper/ProductMapper.java
+++ backend/src/main/java/com/projedata/backend/mapper/ProductMapper.java
@@ -1,7 +1,18 @@
 package com.projedata.backend.mapper;
 
+import com.projedata.backend.model.dto.ProductRequestDto;
+import com.projedata.backend.model.dto.ProductResponsetDto;
+import com.projedata.backend.model.entities.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "compositions", ignore = true)
+    //Request -> Entidade (Ignora ID porque o banco gera)
+    Product toEntity(ProductRequestDto dto);
+
+    //Entidade -> response
+    ProductResponsetDto toDto(Product entity);
 }
---
+++ backend/src/main/java/com/projedata/backend/mapper/RawMaterialMapper.java
@@ -0,0 +1,18 @@
+package com.projedata.backend.mapper;
+
+import com.projedata.backend.model.dto.RawMaterialRequestDTo;
+import com.projedata.backend.model.dto.RawMaterialResponseDTo;
+import com.projedata.backend.model.entities.RawMaterial;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "Spring")
+public interface RawMaterialMapper {
+
+    // Request -> Entidade (Ignora ID porque o banco gera)
+    @Mapping(target = "id", ignore = true)
+    RawMaterial toEntity(RawMaterialRequestDTo dto);
+
+    // Entidade -> Response
+    RawMaterialResponseDTo toResponse(RawMaterial entity);
+}
